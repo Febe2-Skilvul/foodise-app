5d431276994ccd06f60680374eb7e5ddc74f14d5
rioaldie <rioaldierwanto@gmail.com>
2022-11-21 20:31:33 +0700

    add navabr

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Search from '../components/Search';
+import Appbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Appbar />
       <Container
         style={{
           minHeight: '100vh',
+          borderTop: 'solid 2px #D9D9D9',
         }}>
         <div className="d-block d-md-none">
           <Search />
