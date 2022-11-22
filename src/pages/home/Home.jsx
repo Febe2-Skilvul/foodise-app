8ad10d979e5120cc77c9fa9f0345442315bbcc62
rioaldie <rioaldierwanto@gmail.com>
2022-11-22 16:37:48 +0700

    portal service

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import ServicePortal from '../../components/Portal';
 
 const Home = () => {
-  return <div>Home</div>;
+  const user = 'sherlin';
+  return (
+    <Row className="justify-content-md-center">
+      <Col className="mt-5">
+        <ServicePortal user={user !== null ? user : ''} />
+      </Col>
+    </Row>
+  );
 };
 
 export default Home;
