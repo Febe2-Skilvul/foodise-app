12acb910a6580ace9268335d46a258bb9f5abf6a
Rio Aldi Erwanto <75315055+RioAldie@users.noreply.github.com>
2022-11-23 14:19:28 +0700

    Revert "Detail page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
-import Detail from './pages/Detail/Detail';
 import Home from './pages/home/Home';
 import LandingPage from './pages/Landing-page/LandingPage';
 import Signup from './pages/signup/Signup';
@@ -14,7 +13,6 @@ const App = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path="/home" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/home/:id" element={<Detail />} />
           </Routes>
         </Layout>
       </BrowserRouter>
