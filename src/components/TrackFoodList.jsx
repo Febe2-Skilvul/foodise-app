a6b5ea49ab99a45501ff4be8161282119d9a1b0b
rioaldie <rioaldierwanto@gmail.com>
2022-11-24 22:25:30 +0700

    add track list foods

--- src/components/TrackFoodList.jsx
+++ src/components/TrackFoodList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Stack } from 'react-bootstrap';
+import CardFoodAdded from './CardFoodAdded';
 
 const TrackFoodList = () => {
   return (
-    <Stack className=" w-100 justify-content-md-center border border-dark">
-      <div>card food</div>
+    <Stack className="track-food-container">
+      <CardFoodAdded />
+      <CardFoodAdded />
     </Stack>
   );
 };
